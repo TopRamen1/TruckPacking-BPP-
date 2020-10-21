ce5ab88414a926426242a0b81f8f4ef9b729559e
Nicolas Duc <49471089+TopRamen1@users.noreply.github.com>
2020-10-21 23:12:49 +0200

    mathmodel init

--- TruckPackingProblem/src/com/mathmodel/Main.java
+++ TruckPackingProblem/src/com/mathmodel/Main.java
@@ -6,9 +6,24 @@ public class Main {
         System.out.println("BURN IN HELL !!!!");
 
 
+        int k = 5;
+        int[] a = {300, 150, 50};
+        int[] p = {60, 40, 20};
+        int[] s_min = {18, 12, 7};
+        int[] s_max = {28, 17, 12};
+        int[] w = {1, 4, 3, 2};
+        int d = 5;
 
-        MathModel mathModel = new MathModel();
-        mathModel.ObjectiveFcn();
+        int[][] x = {{1,1,0,0},
+                     {0,0,1,1},
+                     {0,0,0,0}};
+
+        int[] y = {1,1,0};
+
+        MathModel mathModel = new MathModel(x,y);
+        int ans = mathModel.ObjectiveFcn(a, p, s_min, s_max, w, d, k);
+
+        System.out.println(ans);
 	// write your code here
     }
 }
