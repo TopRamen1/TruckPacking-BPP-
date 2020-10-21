af9b371c45faedcf72a6794bb089df17b9ee79e9
Nicolas Duc <49471089+TopRamen1@users.noreply.github.com>
2020-10-21 23:11:22 +0200

    Mathmodel init check

--- TruckPackingProblem/src/com/mathmodel/MathModel.java
+++ TruckPackingProblem/src/com/mathmodel/MathModel.java
@@ -5,20 +5,24 @@ public class MathModel {
     public int[][] x;
     public int[] y;
 
-    public int ObjectiveFcn(int[] a, int[] p, int[] s_min, int[] s_max, int[] w, int[] d, int k) {
+    public MathModel(int[][] x_, int[] y_){
+        x = x_;
+        y = y_;
+    }
+    public int ObjectiveFcn(int[] a, int[] p, int[] s_min, int[] s_max, int[] w, int d, int k) {
         int w_sum = 0;
 
         int sum1 = 0,sum2 = 0,sum3 = 0;
         for (int i = 0, y_len = y.length; i < y_len; i++) {
             sum1 = sum1 + (a[i] * y[i]);
 
-            sum2 = sum2 + (d[i] * k * s_min[i] * y[i]);
+            sum2 = sum2 + (d * k * s_min[i] * y[i]);
 
-            for (int j = 0, x_len = x.length; j < x_len; j++) {
+            for (int j = 0, x_len = x[0].length; j < x_len; j++) {
                 w_sum = w_sum + w[j]*x[i][j];
             }
 
-            sum3 = sum3 + (d[i] * k * w_sum / p[i] * (s_max[i] - s_min[i]) * y[i]);
+            sum3 = sum3 + (d * k * w_sum / p[i] * (s_max[i] - s_min[i]) * y[i]);
         }
 
         return sum1 + sum2 + sum3;
