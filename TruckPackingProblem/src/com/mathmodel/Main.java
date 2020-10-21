b27a96e736d89b52f2774ee283391eaee3058120
Nicolas Duc <49471089+TopRamen1@users.noreply.github.com>
2020-10-21 22:32:36 +0200

    Project setup and ObjectiveFCN v1

---
+++ TruckPackingProblem/src/com/mathmodel/Main.java
@@ -0,0 +1,14 @@
+package com.mathmodel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("BURN IN HELL !!!!");
+
+
+
+        MathModel mathModel = new MathModel();
+        mathModel.ObjectiveFcn();
+	// write your code here
+    }
+}
